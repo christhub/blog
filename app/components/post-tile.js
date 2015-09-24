df0bed127a5bed25df30b095e1d7d1514031421e
toma apostolov & christian lei <student@epicodus.com>
2015-09-24 11:02:11 -0700

    modal ed

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    openModal: function() {
+      $("#myModal").modal('show');
+    }
+  }
   // hovering: false,
   // actions: {
   //   something() {
