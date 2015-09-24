0768ba44ab01ba03e9c0865f1b7cde0162b8b177
christian lei & toma apostolov <student@epicodus.com>
2015-09-24 15:01:55 -0700

    MODAL'ed

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -6,6 +6,17 @@ export default Ember.Component.extend({
       $(".modal-title").text(post.get('headline'));
       $("#modal-image").attr("src", post.get('photo'));
       $("#myModal").modal('show');
+
+      var article = post.get('article');
+      var shortArticle = article.substring(0,500);
+
+      $("#modal-article").text(shortArticle);
+
+      var postID = post.get('id');
+      var postURL = "/post/" + postID;
+
+      $("a").attr("href", postURL);
+
     }
   }
   // hovering: false,
